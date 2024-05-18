f2675ba2db4dde5b5bc048e7274e7ff3992c7d63
magley <60552482+magley@users.noreply.github.com>
2024-05-18 14:25:26 +0200

    feat: Simplify navbar
    Instead of being hardcoded, each role has different navbar items ("dropdown" items) with custom icons.

--- app/ises/app/components/navbar.tsx
+++ app/ises/app/components/navbar.tsx
@@ -22,6 +22,13 @@ export default () => {
         onClick: (() => void) | null
     }
 
+    interface DropdownItem {
+        name: string,
+        route: string | null,
+        onClick: (() => void) | null,
+        iconFname: string,
+    }
+
     const navigationGuest: NavigationItem[] = [
         { name: 'Home', route: '/', onClick: null },
         { name: 'Login', route: '/login', onClick: null },
@@ -41,8 +48,22 @@ export default () => {
         { name: 'Home', route: '/', onClick: null },
     ];
 
+    //////////////////////////////////////////////////////////////
+
+    const userDropdownClient: DropdownItem[] = [
+        { name: 'My Account', route: '/account', onClick: null, iconFname: 'cog.png' },
+        { name: 'Purchase History', route: '/history', onClick: null, iconFname: 'cart.png' },
+        { name: 'Sign Out', route: null, onClick: logOut, iconFname: 'log_out.png' },
+    ];
+
+    const userDropdownAdmin: DropdownItem[] = [
+        { name: 'My Account', route: '/account', onClick: null, iconFname: 'cog.png' },
+        { name: 'Sign Out', route: null, onClick: logOut, iconFname: 'log_out.png' },
+    ];
+
     // TODO: This should update whenever the user logs in/logs out.
     const [navigation, setNavigation] = useState(navigationGuest);
+    const [dropdown, setDropdown] = useState<DropdownItem[] | null>(null);
 
     const [loggedIn, setLoggedIn] = useState(false);
 
@@ -56,15 +77,19 @@ export default () => {
         switch (role) {
             case "SuperAdmin": case "Admin":
                 setNavigation(navigationAdmin);
+                setDropdown(userDropdownAdmin);
                 break;
             case "User":
                 setNavigation(navigationClient);
+                setDropdown(userDropdownClient);
                 break;
             case "Full user":
                 setNavigation(navigationFullClient);
+                setDropdown(userDropdownClient);
                 break;
             default:
                 setNavigation(navigationGuest);
+                setDropdown(null);
                 break;
         }
     }, []);
@@ -92,7 +117,7 @@ export default () => {
                     ))
                 }
                 <span>
-                    {loggedIn &&
+                    {(loggedIn && dropdown != null) &&
                         <Menu as="div" className="relative ml-3 inline-block float-right mr-5 mt-2">
                             <div>
                                 <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -127,59 +152,42 @@ export default () => {
                                                 </>
                                             )}
                                         </Menu.Item>
-                                        <Menu.Item>
-                                            {({ active }) => (
-                                                <NavLink
-                                                    to="/account"
-                                                    className={classNames(
-                                                        active ? 'bg-indigo-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}
-                                                >
-                                                    <svg className="h-6 w-6 inline-block mr-2 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                    </svg>
-                                                    My Account
-                                                </NavLink>
-                                            )}
-                                        </Menu.Item>
-                                        <Menu.Item>
-                                            {({ active }) => (
-                                                <NavLink
-                                                    to="#"
-                                                    className={classNames(
-                                                        active ? 'bg-indigo-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}
-                                                >
-                                                    <svg className="h-6 w-6 inline-block mr-2 text-gray-700" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <circle cx="9" cy="19" r="2" />  <circle cx="17" cy="19" r="2" />  <path d="M3 3h2l2 12a3 3 0 0 0 3 2h7a3 3 0 0 0 3 -2l1 -7h-15.2" /></svg>
-                                                    Purchase History
-                                                </NavLink>
-                                            )}
-                                        </Menu.Item>
-                                        <Menu.Item>
-                                            {({ active }) => (
-                                                <button
-                                                    type="submit"
-                                                    onClick={logOut}
-                                                    className={classNames(
-                                                        active ? 'bg-indigo-100 text-gray-900' : 'text-gray-700',
-                                                        'block w-full px-4 py-2 text-left text-sm'
-                                                    )}
-                                                >
-                                                    <svg className="h-6 w-6 inline-block mr-2 text-gray-700" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />  <polyline points="16 17 21 12 16 7" />  <line x1="21" y1="12" x2="9" y2="12" /></svg>
-                                                    Sign out
-                                                </button>
-                                            )}
-                                        </Menu.Item>
+                                        {dropdown.map((item) => (
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    item.route ?
+                                                        <Link
+                                                            key={item.name}
+                                                            to={item.route}
+                                                            className={classNames(
+                                                                active ? 'bg-indigo-100 text-gray-900' : 'text-gray-700',
+                                                                'block px-4 py-2 text-sm'
+                                                            )}>
+                                                            <img src={item.iconFname} className="h-6 w-6 inline-block mr-2 text-gray-700" />
+                                                            {item.name}
+                                                        </Link>
+                                                        :
+                                                        <Link
+                                                            key={item.name}
+                                                            to=""
+                                                            onClick={(e) => { e.preventDefault(); if (item.onClick != null) item.onClick(); }}
+                                                            className={classNames(
+                                                                active ? 'bg-indigo-100 text-gray-900' : 'text-gray-700',
+                                                                'block px-4 py-2 text-sm'
+                                                            )}>
+                                                            <img src={item.iconFname} className="h-6 w-6 inline-block mr-2 text-gray-700" />
+                                                            {item.name}
+                                                        </Link>
+                                                )}
+                                            </Menu.Item>
+                                        ))}
                                     </div>
                                 </Menu.Items>
                             </Transition>
                         </Menu>
                     }
                 </span>
-            </span>
+            </span >
         </nav >
     );
 }

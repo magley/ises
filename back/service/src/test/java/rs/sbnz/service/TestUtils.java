7596538c4f10c439d0764df5e3cc85725617b9e8
magley <60552482+magley@users.noreply.github.com>
2024-05-26 13:55:13 +0200

    fix: Fix test utility method fetching wrong objects from `ksession`

--- back/service/src/test/java/rs/sbnz/service/TestUtils.java
+++ back/service/src/test/java/rs/sbnz/service/TestUtils.java
@@ -9,8 +9,8 @@ import org.kie.api.runtime.KieSession;
 import rs.sbnz.model.Alarm;
 
 public class TestUtils {
-    static <T> List<T> getFactsFrom(KieSession ksession) {
-        Collection<? extends Object> coll = ksession.getObjects(new ClassObjectFilter(Alarm.class));
+    static <T> List<T> getFactsFrom(KieSession ksession, Class<T> clazz) {
+        Collection<? extends Object> coll = ksession.getObjects(new ClassObjectFilter(clazz));
         List<T> li = new ArrayList<>();
         for (Object o : coll) {
             li.add((T)o);
